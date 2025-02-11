e885ecc1a69454c5a4ee1cf12549f4ba111c9e69
AngelMValentin <angelvalentinusmc@gmail.com>
2025-02-10 20:24:38 -0800

    class 1

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=128)
-    subtitle = models.CharField(max_length=256)
-    body = models.TextField()
-    created_on = models.DataTimeField(auto_now_add=True)
+class Post(models.Model):                                   # inheritance
+    title = models.CharField(max_length=128)                # composition
+    subtitle = models.CharField(max_length=256)             # composition
+    body = models.TextField()                               # composition
+    created_on = models.DateTimeField(auto_now_add=True)    # composition
+
+    def __str__(self):
+        return self.title
